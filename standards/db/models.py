e1711b87ca8f5dedcfba4a1fd8fe1e30756972a4
Sébastien Declercq <sebastien.declercq@afnor.org>
2023-05-31 11:47:51 +0200

    db sample script (by chatgpt) + updated model constraint

--- standards/db/models.py
+++ standards/db/models.py
@@ -13,7 +13,9 @@ class Base(DeclarativeBase):
 class Standard(Base):
     __tablename__: str = "standards"
 
-    numdos: Column = Column(String, CheckConstraint("numdos ~ '^[A-Z]\d+$"))
+    numdos: Column = Column(
+        String, CheckConstraint(r"numdos REGEXP '^[A-Z]{2}\d+$'"), primary_key=True
+    )
     files: Mapped[list[File]] = relationship(
         back_populates="standard", cascade="all, delete-orphan"
     )
@@ -22,11 +24,14 @@ class Standard(Base):
 class File(Base):
     __tablename__: str = "files"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     numdosvl: Mapped[str]
-    numdos: Mapped[str] = mapped_column(ForeignKey("standard.numdos"))
+    numdos: Mapped[str] = mapped_column(ForeignKey("standards.numdos"))
     standard: Mapped[Standard] = relationship(back_populates="files")
 
-    __table_args__: CheckConstraint = CheckConstraint(
-        "SUBSTRING(numdosvl, 2, 1) = SUBSTRING(numdos, 2, 1)",
+    __table_args__: tuple[CheckConstraint, ...] = tuple(
+        CheckConstraint(
+            "SUBSTRING(numdosvl, 2, 1) = SUBSTRING(numdos, 2, 1)",
+        )
     )
